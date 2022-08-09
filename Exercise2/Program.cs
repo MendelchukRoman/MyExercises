43a1a0de5ca7d071c0c54c03c6aac54563fa1e8c
kadzuki <heaven.roma@mail.ru>
2022-08-10 00:05:33 +0300

    Задача2

---
+++ Exercise2/Program.cs
@@ -0,0 +1,13 @@
+﻿// На вход принимает два числа, и выдает какое больше а какое меньше.
+Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int Max = 0;
+if (a>b)
+{
+    Max = a;
+}
+else 
+    Max = b;
+    Console.WriteLine($"Наибольшее: {Max} =)");
