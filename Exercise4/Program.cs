b75f921c6415ec18c264e79cc4d0df4075f8d446
kadzuki <heaven.roma@mail.ru>
2022-08-10 00:51:44 +0300

    Задача4

---
+++ Exercise4/Program.cs
@@ -0,0 +1,18 @@
+﻿// На вход принимает 3 случайных числа и выдает максимальное
+
+int [] array = new int [3];
+int max = 0;
+for (int i=0; i<array.Length; i++)
+{
+  array[i] = new Random().Next(1, 11); 
+  Console.WriteLine($"{array[i]}");
+  int number =  array[i];
+  if(number > max)
+   {
+    max = number;
+   }
+}
+Console.WriteLine($"наибольшее {max}");
+
+
+
