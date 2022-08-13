6e2ce5f41a49ea13e7d52e3727b09f43adfd4b54
kadzuki <heaven.roma@mail.ru>
2022-08-13 21:44:21 +0300

    Задача3

---
+++ Exercise3/Program.cs
@@ -0,0 +1,42 @@
+﻿// По заданному номеру дня недели вывести его название
+
+// 1) Назначит числа для дней недели
+// 2) На вводе числа показать день недели
+
+Console.WriteLine("Введите число: ");
+int q = Convert.ToInt32(Console.ReadLine());
+if (q == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else if (q == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else if (q == 3)
+{
+    Console.WriteLine("Среда");
+}
+else if (q == 4)
+{
+    Console.WriteLine("Черверг");
+}
+else if (q == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else if (q == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else if (q == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+
+ 
+
+
+
+ 
+
