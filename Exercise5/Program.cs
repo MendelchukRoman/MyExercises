25e81042747021c06b31ef61ecb0c49469f33ad7
kadzuki <heaven.roma@mail.ru>
2022-08-13 23:46:51 +0300

    Задача5

---
+++ Exercise5/Program.cs
@@ -0,0 +1,24 @@
+﻿// Написать программу вычисления значения функции y = f(a). Допустим, такой:
+// у = x – 2, если x > 0,
+// y = 0, если x = 0,
+// y = |x|, если x < 0.
+int x;
+int y;
+Console.Write ("Число x: ");
+x = Convert.ToInt32(Console.ReadLine ());
+if (x>0)
+{
+    y = x - 2;
+    Console.WriteLine ($"Число y: {y} ");
+}
+else if(x == 0)
+{
+    y = 0;
+    Console.WriteLine ($"Число y: {y} ");
+}
+else if (x < 0)
+{
+    y = (x*(-1));
+    Console.WriteLine ($"Число y: {y} ");
+}
+
