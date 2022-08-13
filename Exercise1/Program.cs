f4cc817155cba9b3a8c08dee4869d9ece4f48500
kadzuki <heaven.roma@mail.ru>
2022-08-13 18:22:12 +0300

    Задача1

---
+++ Exercise1/Program.cs
@@ -0,0 +1,18 @@
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+// 1)Задать 2 числа
+// 2)Проверить является ли квадратом
+
+
+int a = new Random().Next(1, 99);
+int b = new Random().Next(1, 99);
+int qub = b*b;
+Console.WriteLine ($"Первое число {a}");
+Console.WriteLine ($"Второе число {b}");
+if (a == qub)
+{
+   Console.WriteLine($"{a} является квадратом {b}: {qub}");
+}
+else 
+{
+    Console.WriteLine($"{a} не является квадратом {b}");
+}
