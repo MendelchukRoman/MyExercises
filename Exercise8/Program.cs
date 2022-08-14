36c66c05b48b0f94a6a2d6393400b6e3013fc902
kadzuki <heaven.roma@mail.ru>
2022-08-14 19:27:09 +0300

    задача8

---
+++ Exercise8/Program.cs
@@ -0,0 +1,19 @@
+﻿// Показать четные числа от 1 до N
+Console.Write("Массив: ");
+int [] array = new int [10];
+Random num = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+array[i] = num.Next(1, 21);
+Console.Write($"{array[i]+ " "}");
+}
+Console.WriteLine();
+Console.Write("Четные числа: ");
+ for (int i = 0; i < array.Length; i++)
+{
+   if (array[i] % 2 == 0)
+   {
+   Console.Write($"{array[i]+ " "}");
+   }
+}
+
