5e0ff0f99c5971174dcc850d96a211e28fd994a9
kadzuki <heaven.roma@mail.ru>
2022-08-14 15:59:56 +0300

    Задача6

---
+++ Exercise6/Program.cs
@@ -0,0 +1,10 @@
+﻿// Выяснить является ли число чётным
+Random x = new Random();
+int num = x.Next(1, 99);
+Console.WriteLine($"Число: {num} ");
+//int num = Convert.ToInt32(Console.ReadLine()); //new Random().Next(1, 99);
+if (num % 2 == 0)
+{
+    Console.WriteLine($"Число четное");
+}
+else Console.WriteLine($"Число нечетное");
