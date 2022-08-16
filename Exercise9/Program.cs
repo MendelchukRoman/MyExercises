80ac6a23e7e4df124314b8b054ca14d585aea696
kadzuki <heaven.roma@mail.ru>
2022-08-16 20:45:19 +0300

    Задача9

---
+++ Exercise9/Program.cs
@@ -0,0 +1,17 @@
+﻿// Показать последнюю цифру трёхзначного числa
+
+
+static void last ()
+{
+Console.WriteLine("Введите число: ");
+string text = Console.ReadLine();
+int num = text[text.Length - 1] - '0';
+Console.WriteLine("Последняя цифра:{0,3}", num);
+}
+
+last();
+
+
+
+
+
